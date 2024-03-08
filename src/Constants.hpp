412736d0491dbcbe0d886e0737c1473b6df2e893
fordcars <corteocarl@gmail.com>
2024-03-08 12:49:06 -0500

    Added constants

---
+++ src/Constants.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace Constants {
+    constexpr unsigned OPENGL_MAJOR_VERSION = 4;
+    constexpr unsigned OPENGL_MINOR_VERSION = 3;
+    constexpr const char* GAME_NAME = "Vroom!";
+    constexpr unsigned SIZE_X = 1024;
+    constexpr unsigned SIZE_Y = 800;
+}
--- src/Game.cpp
+++ src/Game.cpp
@@ -2,6 +2,7 @@
 #include <SDL2/SDL.h>
 
 #include "Log.hpp"
+#include "Constants.hpp"
 
 Game::Game() {}
 
@@ -13,5 +14,26 @@ bool Game::init() {
 		Log::sdl_error() << "Unable to initialize SDL";
         return false;
 	}
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, Constants::OPENGL_MAJOR_VERSION);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, Constants::OPENGL_MINOR_VERSION);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+	mMainWindow = SDL_CreateWindow(
+        Constants::GAME_NAME,
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        Constants::SIZE_X,
+        Constants::SIZE_Y,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN
+    );
+	
+	if(!mMainWindow)
+	{
+		Log::sdl_error() << "Unable to create window";
+        return false;
+	}
+
     return true;
 }
