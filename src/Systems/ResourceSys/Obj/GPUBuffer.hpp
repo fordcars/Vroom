52a225407155a192f91e6492ef2639a73a465f67
fordcars <corteocarl@gmail.com>
2024-03-18 01:49:15 -0400

    Fixed GPU buffer size calculation

--- src/Systems/ResourceSys/Obj/GPUBuffer.hpp
+++ src/Systems/ResourceSys/Obj/GPUBuffer.hpp
@@ -19,8 +19,8 @@ public:
     template<typename T>
     void setData(const std::vector<T>& data) {
         if(data.empty()) return;
-        mSize = data.size() & sizeof(T);
-        
+        mSize = data.size() * sizeof(T);
+
         glBindBuffer(GL_ARRAY_BUFFER, mId);
         glBufferData(
             GL_ARRAY_BUFFER,
